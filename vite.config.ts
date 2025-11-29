ec8398ac95729d3e281fef9cd8781b35ab8e5fa9
Ron-Caster <hprsd21@gmail.com>
2025-11-30 02:22:29 +0530

    config: Set Vite base URL to root path.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Pmats-Site/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
